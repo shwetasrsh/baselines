03bb665daa71ac6aee870cebfdccf14402635b1e
shwetasrsh <shwetasrsh@gmail.com>
2020-04-07 16:23:04 +0530

    Update tf_util.py

--- baselines/common/tf_util.py
+++ baselines/common/tf_util.py
@@ -1,5 +1,6 @@
 import numpy as np
-import tensorflow as tf  # pylint: ignore-module
+#import tensorflow as tf  # pylint: ignore-module
+import tensorflow.compat.v1 as tf
 import copy
 import os
 import functools
